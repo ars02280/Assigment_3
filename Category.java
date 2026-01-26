83b9e7132bc589fc7912ead906470d6ed44e4327
ars02280 <spakspak01@gmail.com>
2026-01-27 02:55:00 +0500

    category added

---
+++ Category.java
@@ -0,0 +1,12 @@
+public class Category {
+    private int id;
+    private String name;
+
+    public Category(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+}
