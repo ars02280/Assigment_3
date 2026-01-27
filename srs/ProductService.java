b8e709a06d78ee2ab4ae068a880404f791fffdf9
ars02280 <spakspak01@gmail.com>
2026-01-27 08:04:45 +0500

    added repository

--- srs/ProductService.java
+++
@@ -1,8 +0,0 @@
-public class ProductService {
-    private final ProductRepository repo = new ProductRepository();
-
-    public void addProduct(Product product) {
-        product.validate();
-        repo.create(product);
-    }
-}
