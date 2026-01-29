5266a90b4b4f588c04a7ae39da27b5055e3cec87
ars02280 <spakspak01@gmail.com>
2026-01-29 07:14:30 +0500

    polish

---
+++ srs/resources/schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE categories (
+                            id SERIAL PRIMARY KEY,
+                            name VARCHAR(50) UNIQUE NOT NULL
+);
+
+CREATE TABLE products (
+                          id SERIAL PRIMARY KEY,
+                          name VARCHAR(50) NOT NULL,
+                          price NUMERIC NOT NULL,
+                          type VARCHAR(20) NOT NULL,
+                          category_id INT NOT NULL,
+                          FOREIGN KEY (category_id) REFERENCES categories(id)
+);
